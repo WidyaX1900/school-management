0d217d47a3b9bec32f5a9fca2dd45c9cf464ae6d
WidyaX1900 <tuderangga93@gmail,com>
2025-11-16 12:57:13 +0800

    view: front page content

--- resources/views/templates/dashboard.blade.php
+++ resources/views/templates/dashboard.blade.php
@@ -1,1 +1,37 @@
-<x-layout></x-layout>
+<x-layout>
+    <div class="grid grid-cols-3 gap-4">
+        <div class="w-full p-5 rounded bg-blue-950 text-slate-50 flex items-center">
+            <div class="w-16 h-16 bg-blue-900 rounded-[50%] flex items-center justify-center me-4">
+                <span class="material-icons">
+                    <span class="text-4xl">group</span>
+                </span>
+            </div>
+            <div>
+                <h2 class="text-3xl font-bold">9.999.999.999</h2>
+                <p>Students</p>
+            </div>
+        </div>
+        <div class="w-full p-5 rounded bg-blue-950 text-slate-50 flex items-center">
+            <div class="w-16 h-16 bg-blue-900 rounded-[50%] flex items-center justify-center me-4">
+                <span class="material-icons">
+                    <span class="text-4xl">group</span>
+                </span>
+            </div>
+            <div>
+                <h2 class="text-3xl font-bold">9.999.999.999</h2>
+                <p>Teachers</p>
+            </div>
+        </div>
+        <div class="w-full p-5 rounded bg-blue-950 text-slate-50 flex items-center">
+            <div class="w-16 h-16 bg-blue-900 rounded-[50%] flex items-center justify-center me-4">
+                <span class="material-icons">
+                    <span class="text-4xl">meeting_room</span>
+                </span>
+            </div>
+            <div>
+                <h2 class="text-3xl font-bold">9.999.999.999</h2>
+                <p>Classes</p>
+            </div>
+        </div>
+    </div>
+</x-layout>
